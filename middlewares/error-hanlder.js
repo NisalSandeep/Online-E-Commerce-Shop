5683934e3a138fdebb8725307d3d017500740dfe
NisalSandeep <sandeep-as22018@stu.kln.ac.lk>
2025-11-01 16:40:19 +0530

    add error code 404 implementation

--- middlewares/error-hanlder.js
+++ middlewares/error-hanlder.js
@@ -1,5 +1,9 @@
 function handleError(error, req, res, next) { // whenever the other middlwares or things fail this will be called
     console.error(error);
+
+    if(error.code === 404) {
+        return res.status(404).render('shared/404'); 
+    }
     res.status(500).render('shared/500');
 }
 
